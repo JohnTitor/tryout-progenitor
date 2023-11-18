fc23cdc7db1788a275c11ab544168086a80ec741
Yuki Okushi <jtitor@2k36.org>
2023-11-18 10:30:36 +0900

    license

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ pub use reqwest::Client;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     progenitor::generate_api!("./docs/openapi.json");
 
-    let client = Client::new("localhost:8080");
-    let foo = client.api_version();
-    println!("{foo}");
+    let client = Client::new("http://localhost:8080");
+    let api_version = client.api_version();
+    let todo_list = client.get_todos().await?;
+    println!("api version: {api_version}\ntodo list: {todo_list:?}");
     Ok(())
 }
